3aed0b68c748ac8a1047ab1a913727ad84eee9d8
cyberohn2 <111274485+cyberohn2@users.noreply.github.com>
2022-11-18 14:38:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ request.addEventListener('readystatechange', () => {
     var response = JSON.parse(request.responseText);
       let i = Math.floor((Math.random() * response.length - 1));
   document.getElementById('response').innerHTML = response[i].text + "<br> <br> By:" + response[i].author ;
+  }else if(request.status != 200 ){
+    document.getElementById('response').innerHTML = "could not fetch quotes"
   }
 });
 request.open('GET', "https://type.fit/api/quotes");
